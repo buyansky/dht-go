d23210b8f8d92377162b0f859cda87d180058491
buyansky <816435@qq.com>
2019-09-11 12:17:40 +0800

    88

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main()  {
 
-	uip:=net.ParseIP("127.0.0.1")
+	uip:=net.ParseIP("132.232.88.200")
 	udpaddr:=net.UDPAddr{IP:uip,Port:36556}
 	listener,e:= net.ListenUDP("udp",&udpaddr)
 
